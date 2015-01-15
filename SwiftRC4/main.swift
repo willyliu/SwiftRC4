e68e78e7e2d4bddbf2504c357f83da484307ed7c
Willy Liu <willyliu@kkbox.com>
2015-01-15 18:11:12 +0800

    Initial Commit

---
+++ SwiftRC4/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SwiftRC4
+//
+//  Created by Willy Liu on 2015/1/15.
+//  Copyright (c) 2015年 Willy Liu. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
